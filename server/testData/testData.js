f163b6aed615ffe0e6e3d4a6a2383d16509f52f1
storatus <sebastian.glahn89@web.de>
2026-03-07 12:16:39 -0400

    feat: add initial sample product data to `testData.js`

---
+++ server/testData/testData.js
@@ -0,0 +1,62 @@
+const sampleProducts = [
+    {
+        img: "https://placeholder.pagebee.io/api/random/400/300",
+        brand: "Nike",
+        title: "Nike Kids",
+        rating: 4.8,
+        reviews: 520,
+        sellPrice: 52.99,
+        orders: "245",
+        mrp: "69.99",
+        discount: 24,
+        category: "child"
+    },
+    {
+        img: "https://placeholder.pagebee.io/api/random/400/300",
+        brand: "Skechers",
+        title: "Skechers",
+        rating: 4.6,
+        reviews: 310,
+        sellPrice: 48.00,
+        orders: "180",
+        mrp: "59.99",
+        discount: 20,
+        category: "child"
+    },
+    {
+        img: "https://placeholder.pagebee.io/api/random/400/300",
+        brand: "Puma",
+        title: "Puma",
+        rating: 4.2,
+        reviews: 210,
+        sellPrice: 48.00,
+        orders: "120",
+        mrp: "59.99",
+        discount: 20,
+        category: "child"
+    },
+    {
+        img: "https://placeholder.pagebee.io/api/random/400/300",
+        brand: "Adidas",
+        title: "Adidas",
+        rating: 4.8,
+        reviews: 133,
+        sellPrice: 38.00,
+        orders: "120",
+        mrp: "59.99",
+        discount: 20,
+        category: "men"
+    },
+    {
+        img: "https://placeholder.pagebee.io/api/random/400/300",
+        brand: "Skechers",
+        title: "Skechers",
+        rating: 4.6,
+        reviews: 310,
+        sellPrice: 48.00,
+        orders: "180",
+        mrp: "59.99",
+        discount: 20,
+        category: "women"
+    },
+]
